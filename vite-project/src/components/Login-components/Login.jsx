e2df412a9161513d498f50e4468c0f510569f973
Yashwanth Pendem <yashwanthpendem05@gmail.com>
2025-07-15 15:56:22 +0530

    Made changes

--- vite-project/src/components/Login-components/Login.jsx
+++ vite-project/src/components/Login-components/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef} from "react";
 import { useSearchParams, Link, useNavigate } from "react-router-dom";
 import Cookies from "js-cookie"
 import "./Login.css";
@@ -7,7 +7,36 @@ const Login = () => {
     const [searchParams] = useSearchParams();
     const mode = searchParams.get("mode");
     const [isLogin, setIsLogin] = useState(true);
+    const [first, onFirst] = useState(true);
     const navigate = useNavigate();
+    const loginRef = useRef(null);
+    const registerRef = useRef(null);
+    const parentRef = useRef(null);
+
+    useEffect(() => {
+        if(mode === "register") {
+            if (parentRef.current && registerRef.current) {
+                const height = registerRef.current.getBoundingClientRect().height;
+                if(first){
+                    parentRef.current.style.height = `${height}px`;
+                    onFirst(false);
+                }
+                else
+                parentRef.current.style.height = `${height+100}px`;
+                console.log("Child height:", height);
+            } else {
+                console.warn("childRef is null");
+            }
+        }else {
+            if (loginRef.current && parentRef.current) {
+                const height = loginRef.current.clientHeight;
+                parentRef.current.style.height = `${height}px`;
+                console.log("Child height:", height);
+            } else {
+                console.warn("childRef is null");
+            }
+        }
+    }, [mode]);
 
     useEffect(() => {
         setIsLogin(mode !== "register");
@@ -94,7 +123,7 @@ const Login = () => {
                     phonenumber: "",
                     fullname: "",
                     location: "",
-                  });
+                });
             } else {
                 alert(`Error: ${result.msg || "Registration failed"}`);
             }
@@ -106,100 +135,100 @@ const Login = () => {
 
     return (
         <div className="login-container">
-            <div className={`wrapper ${isLogin ? "" : "active"}`}>
+            <div ref={parentRef} className={`wrapper ${isLogin ? "" : "active"}`}>
                 {/* Login Form */}
-                
-                    <div className="login-form">
-                        <form onSubmit={handleLoginSubmit}>
-                            <h2>Login</h2>
-                            <div className="input-field">
-                                <input type="text" name="check" value={loginData.check} onChange={handleLoginChange} required />
-                                <label>Username / Email / Phone</label>
-                            </div>
-                            <div className="input-field">
-                                <input type="password" name="password" value={loginData.password} onChange={handleLoginChange} required />
-                                <label>Password</label>
-                            </div>
-                            <div className="forget">
-                                <label htmlFor="remember">
-                                    <input type="checkbox" id="remember" />
-                                    <p>Remember me</p>
-                                </label>
-                                <a href="#">Forgot password?</a>
-                            </div>
-                            <div className="submit">
-                                <button type="submit">Log In</button>
-                            </div>
-                            <div className="register">
-                                <p>
-                                    Don't have an account?{" "}
-                                    <Link className="switch" to="/login?mode=register" onClick={() => setIsLogin(false)}>
-                                        Register
-                                    </Link>
-                                </p>
-                            </div>
-                        </form>
-                    </div>
-                
+
+                <div ref={loginRef} className="login-form">
+                    <form onSubmit={handleLoginSubmit}>
+                        <h2>Login</h2>
+                        <div className="input-field">
+                            <input type="text" name="check" value={loginData.check} onChange={handleLoginChange} required />
+                            <label>Username / Email / Phone</label>
+                        </div>
+                        <div className="input-field">
+                            <input type="password" name="password" value={loginData.password} onChange={handleLoginChange} required />
+                            <label>Password</label>
+                        </div>
+                        <div className="forget">
+                            <label htmlFor="remember">
+                                <input type="checkbox" id="remember" />
+                                <p>Remember me</p>
+                            </label>
+                            <a href="#">Forgot password?</a>
+                        </div>
+                        <div className="submit">
+                            <button type="submit">Log In</button>
+                        </div>
+                        <div className="register">
+                            <p>
+                                Don't have an account?{" "}
+                                <Link className="switch" to="/login?mode=register" onClick={() => setIsLogin(false)}>
+                                    Register
+                                </Link>
+                            </p>
+                        </div>
+                    </form>
+                </div>
+
 
                 {/* Register Form */}
-                
-                    <div className="register-form">
-                        <form onSubmit={handleRegisterSubmit}>
-                            <h2>Register</h2>
 
-                            <div className="input-field">
-                                <input type="text" name="fullname" value={registerData.fullname} onChange={handleRegisterChange} required />
-                                <label>Full Name</label>
-                            </div>
+                <div ref={registerRef} className="register-form" autoComplete="off">
+                    <form onSubmit={handleRegisterSubmit}>
+                        <h2>Register</h2>
 
-                            <div className="input-field">
-                                <input type="text" name="username" value={registerData.username} onChange={handleRegisterChange} required />
-                                <label>Username</label>
-                            </div>
+                        <div className="input-field">
+                            <input type="text" name="fullname" value={registerData.fullname} onChange={handleRegisterChange} autoComplete="off" required />
+                            <label>Full Name</label>
+                        </div>
 
-                            <div className="input-field">
-                                <input type="email" name="email" value={registerData.email} onChange={handleRegisterChange} required />
-                                <label>Email</label>
-                            </div>
+                        <div className="input-field">
+                            <input type="text" name="username" value={registerData.username} onChange={handleRegisterChange} autoComplete="off" required />
+                            <label>Username</label>
+                        </div>
 
-                            <div className="input-field">
-                                <input type="tel" name="phonenumber" pattern="[0-9]{10}" value={registerData.phonenumber} onChange={handleRegisterChange} required />
-                                <label>Phone Number</label>
-                            </div>
+                        <div className="input-field">
+                            <input type="email" name="email" value={registerData.email} onChange={handleRegisterChange} autoComplete="off" required />
+                            <label>Email</label>
+                        </div>
 
-                            <div className="input-field">
-                                <input type="password" name="password" value={registerData.password} onChange={handleRegisterChange} required />
-                                <label>Password</label>
-                            </div>
+                        <div className="input-field">
+                            <input type="tel" name="phonenumber" pattern="[0-9]{10}" value={registerData.phonenumber} autoComplete="off" onChange={handleRegisterChange} required />
+                            <label>Phone Number</label>
+                        </div>
 
-                            <div className="input-field">
-                                <input type="password" name="confirmPassword" value={registerData.confirmPassword} onChange={handleRegisterChange} required />
-                                <label>Confirm Password</label>
-                            </div>
+                        <div className="input-field">
+                            <input type="password" name="password" value={registerData.password} autoComplete="new-password" onChange={handleRegisterChange} required />
+                            <label>Password</label>
+                        </div>
 
-                            <div className="forget">
-                                <label htmlFor="terms">
-                                    <input type="checkbox" id="terms" required />
-                                    <p>I agree to the terms and conditions</p>
-                                </label>
-                            </div>
+                        <div className="input-field">
+                            <input type="password" name="confirmPassword" value={registerData.confirmPassword} autoComplete="off" onChange={handleRegisterChange} required />
+                            <label>Confirm Password</label>
+                        </div>
 
-                            <div className="submit">
-                                <button type="submit">Register</button>
-                            </div>
+                        <div className="forget">
+                            <label htmlFor="terms">
+                                <input type="checkbox" id="terms" required />
+                                <p>I agree to the terms and conditions</p>
+                            </label>
+                        </div>
 
-                            <div className="login">
-                                <p>
-                                    Already have an account?
-                                    <Link className="switch" to="/login?mode=login" onClick={() => setIsLogin(true)}>
-                                        Login
-                                    </Link>
-                                </p>
-                            </div>
-                        </form>
-                    </div>
-                
+                        <div className="submit">
+                            <button type="submit">Register</button>
+                        </div>
+
+                        <div className="login">
+                            <p>
+                                Already have an account?
+                                <Link className="switch" to="/login?mode=login" onClick={() => setIsLogin(true)}>
+                                    Login
+                                </Link>
+                            </p>
+                        </div>
+                    </form>
+                </div>
+
             </div>
         </div>
     );
