7fd7a96e84e8a490e2e746f5f1c8c4528697eb4f
Manisharan73 <manisharanreddyvancha@gmail.com>
2025-07-15 19:14:27 +0530

    Changed banner

--- vite-project/src/components/Banner.jsx
+++ vite-project/src/components/Banner.jsx
@@ -6,17 +6,15 @@ import { useNavigate } from 'react-router-dom';
 
 const Banner = ({ data }) => {
     //const images = ["https://assets-in.bmscdn.com/iedb/movies/images/mobile/listing/xxlarge/kannappa-et00377025-1750744616.jpg", "https://assets-in.bmscdn.com/iedb/movies/images/mobile/listing/xxlarge/f1-the-movie-et00403839-1750674185.jpg"];
-    const images = data && data.map(item => item.backdropurl);
     const [curr, setIndex] = useState(0);
     const navigate = useNavigate();
-
     const handlePrev = () => {
-        setIndex((prev) => (prev === 0 ? images.length - 1 : prev - 1));
+        setIndex((prev) => (prev === 0 ? data.length - 1 : prev - 1));
 
     };
 
     const handleNext = () => {
-        setIndex((next) => (next === images.length - 1 ? 0 : next + 1));
+        setIndex((next) => (next === data.length - 1 ? 0 : next + 1));
 
     };
 
@@ -33,12 +31,16 @@ const Banner = ({ data }) => {
                         className="slider-track"
                         style={{ transform: `translateX(-${curr * 100}%)` }}
                     >
-                        {images && images.map((image, index) => (
+                        {data && data.map((item, index) => (
                             <div
                                 key={index}
                                 className="slide"
-                                style={{ backgroundImage: `url(${image})` }}
-                            />
+                                style={{ backgroundImage: `url(${item.backdropurl})` }}
+                            >
+                                <div className="banner-text">
+                                    <h1>{item.title}</h1>
+                                </div>
+                            </div>
                         ))}
                     </div>
                 </div>
